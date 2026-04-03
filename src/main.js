2f7fec0c605d2663e0dbf543a061b7221923e472
Simeon Jackman <simeon@jackman.ch>
2026-04-03 09:39:37 +0000

    ADD: 404

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import IntroductionView from './views/IntroductionView.vue'
 import DataUploaderView from './views/DataUploaderView.vue'
 import DataAnalysisView from './views/DataAnalysisView.vue'
 
+// Recover deep links redirected by public/404.html on static hosting.
+const redirectedPath = new URLSearchParams(window.location.search).get('redirect')
+if (redirectedPath) {
+    window.history.replaceState(null, '', redirectedPath)
+}
+
 const routes = [
     { path: '/', redirect: '/introduction' },
     { path: '/introduction', name: 'introduction', component: IntroductionView },
